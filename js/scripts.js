c745d5081dd18841e6df173f9957c398f78bc407
Tom Schisler <tschisler8579@gmail.com>
2017-02-28 14:32:51 -0500

    Add CSS & JavaScript

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+jQuery(function($) {
+
+    'use strict';
+
+    var HOME;
+
+    HOME = {
+        init: function() {
+            // this.slideshow();
+        },
+        testing: function() {
+            $('body').each(function() {
+              console.log("testing");
+            });
+        }
+    };
+    HOME.init();
+
+});
