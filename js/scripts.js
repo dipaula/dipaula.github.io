f8d34ec6b4be36457d29f1de5872f9dd88ef1e2c
Tom Schisler <tschisler8579@gmail.com>
2017-02-28 14:38:54 -0500

    Setup Nav with JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,29 @@ jQuery(function($) {
     'use strict';
 
     var HOME;
+    var NAV;
+
+    NAV = {
+        init: function() {
+            this.setup();
+        },
+        testing: function() {
+            $('nav').each(function() {
+              $(this).append(
+                '<ul>'+
+                  '<li><a href="/">Home</a></li>'+
+                  '<li><a href="/about">About</a></li>'+
+                  '<li><a href="/products">Products</a></li>'+
+                '</ul>';
+              );
+            });
+        }
+    };
+    NAV.init();
 
     HOME = {
         init: function() {
-            // this.slideshow();
+            this.testing();
         },
         testing: function() {
             $('body').each(function() {
@@ -15,5 +34,6 @@ jQuery(function($) {
         }
     };
     HOME.init();
+
 
 });
